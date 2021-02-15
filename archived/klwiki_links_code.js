cac872fb19e543106f481e269fbb1a6dfa83e55d
jinlei <sysserkl@gmail.com>
2021-02-15 09:07:53 +0800

    udpate: 2021-02-15 09:07:52

--- archived/klwiki_links_code.js
+++ archived/klwiki_links_code.js
@@ -1,5 +1,5 @@
 var recent_info_list=[
-["","","","更新日期：2021-02-14 10:18:07 日"],
+["","","","更新日期：2021-02-14 10:20:41 日"],
 ["https://mail.python.org/pipermail/python-list/2005-May/342496.html","20050506 | [HELP] Tkinter Application Minimized to System Tray :)"],
 ["http://m.blog.csdn.net/article/details?id=1811343","20071004 | Tkinter教程之Text(1)篇"],
 ["http://m.blog.csdn.net/article/details?id=1811347","20071004 | Tkinter教程之Text(2)篇"],
