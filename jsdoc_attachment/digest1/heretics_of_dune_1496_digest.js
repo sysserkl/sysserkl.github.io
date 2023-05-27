19dc774871330ed3af6ff41014a5f3e6a47924da
jinlei <sysserkl@gmail.com>
2023-05-27 09:01:42 +0800

    update: 2023-05-27 09:01:40

--- jsdoc_attachment/digest1/heretics_of_dune_1496_digest.js
+++ jsdoc_attachment/digest1/heretics_of_dune_1496_digest.js
@@ -1,2 +1,23 @@
 var digest_global=`
+*renewal
+*brimstone
+*conjoin
+*discoloration
+*eightfold
+*genotype
+*helical
+*hibiscus
+*huntress
+*microbe
+*millennial
+*organizational
+*repeatable
+*salable
+*self-image
+*shipload
+*single-mindedly
+*steersman
+*stunner
+*tableware
+*trainee
 `.trim().split('\n');
