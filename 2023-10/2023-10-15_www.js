f648d841ed5d9319f1ecb697b134f78126c626b9
jinlei <wy7305e@hotmail.com>
2023-10-15 08:56:37 +0800

    update: 2023-10-15 08:56:30

---
+++ 2023-10/2023-10-15_www.js
@@ -0,0 +1,10 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=76337","OpenWrt 23.05.0 释出","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76338","因用户翻译的语言含有仇恨言论 Canonical 移除了 Ubuntu 23.10 镜像","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76339","微软宣布完成对动视暴雪的收购","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76340","Twitter/X 被控非法解雇挑战重返办公室政策的员工","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76341","中资比特币矿场在美引发国家安全担忧","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76342","联想准备推出运行 Android 定制操作系统的设备","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76344","2023 年 Q3 PC 出货量跌势放缓，但苹果暴跌逾二成","2023-10-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76345","Herculaneum 纸草卷轴首个文字在 AI 帮助下成功识别","2023-10-15","Solidot","1",],
+];
