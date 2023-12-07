889a3b6b2e303d965b884f9a1df60c9785121017
jinlei <wy7305e@hotmail.com>
2023-12-07 08:42:00 +0800

    update: 2023-12-07 08:41:56

---
+++ 2023-12/2023-12-07_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=76819","新理论尝试统一广义相对论和量子力学","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76818","Meta 和 IBM 推动 AI 开源","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76817","研究发现租房对衰老的影响大于抽烟和肥胖","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76816","维基百科上的有毒评论与编辑活动下降相关联","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76815","全球教育评估中美国学生数学得分下降","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76814","中国科学家在城市微观气象预报上取得突破","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76813","印度月球探测器推进模块返回地球轨道","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76812","帽带企鹅每天打盹数千次但每次只持续数秒","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76811","ChatGPT 是维基百科 2023 年浏览量最高的条目","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76810","Windows 10 将向个人用户提供额外的三年付费支持","2023-12-07","Solidot","1",],
+["https://www.solidot.org/story?sid=76809","拼多多重塑中国电商格局","2023-12-07","Solidot","1",],
+];
