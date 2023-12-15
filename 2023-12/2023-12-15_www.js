16731f778f78a86501c8a211618e8e872a51f716
jinlei <wy7305e@hotmail.com>
2023-12-15 08:51:09 +0800

    update: 2023-12-15 08:51:06

---
+++ 2023-12/2023-12-15_www.js
@@ -0,0 +1,9 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=76888","考古学家发现距今八千年的要塞","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76886","Threads 开始测试 ActivityPub 整合","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76885","泰议员因推文冒犯君主罪被判 6 年","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76884","澳大利亚在全球率先禁止工程石","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76883","Google 地图将允许用户将位置数据本地储存而非云端","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76882","Ubuntu 24.04 LTS 将默认启用帧指针","2023-12-15","Solidot","1",],
+["https://www.solidot.org/story?sid=76881","微软发布运行在 Linux 上的 Windows AI Studio 预览版","2023-12-15","Solidot","1",],
+];
