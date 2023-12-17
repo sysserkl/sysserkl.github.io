759d6a0b38621e44de958b342555226c22219239
jinlei <wy7305e@hotmail.com>
2023-12-17 08:59:27 +0800

    update: 2023-12-17 08:59:22

---
+++ 2023-12/2023-12-17_www.js
@@ -0,0 +1,4 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=76899","Firefox Android 版支持逾 450 个扩展","2023-12-17","Solidot","1",],
+["https://www.solidot.org/story?sid=76898","Stability AI 发布单幅图像生成 3D 对象的 Stable Zero123","2023-12-17","Solidot","1",],
+];
