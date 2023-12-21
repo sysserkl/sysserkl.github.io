2ceee8aa5a671a5fcfdfb2b3249b7e6b3feeabfd
jinlei <wy7305e@hotmail.com>
2023-12-21 08:58:28 +0800

    update: 2023-12-21 08:58:22

---
+++ 2023-12/2023-12-21_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=76940","LAION 机器学习数据库发现包含儿童色情","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76939","人类大脑细胞在信号传递上用掉了大量能量","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76938","研究人员发现多元化种植相比单一栽培的好处","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76937","微软发布工具修复错误安装到系统中的惠普打印软件","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76936","随着昆虫数量的减少植物花演化出自我授粉","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76935","加拿大将在 2035 年前停售燃油汽车卡车","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76934","勒索组织泄露索尼工作室 Insomniac Games 的 1.67TB 内部文件","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76933","没人知道互联网正在发生什么事","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76932","丰田大发因测试违规停止全部车型出货","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76931","Qubes OS 4.2.0 释出","2023-12-21","Solidot","1",],
+["https://www.solidot.org/story?sid=76930","NASA 用猫咪视频演示深空激光高速传输","2023-12-21","Solidot","1",],
+];
