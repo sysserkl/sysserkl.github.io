03c1f9dc3d83bdc2e457f56b2909442c4b5ab127
jinlei <wy7305e@hotmail.com>
2024-01-03 08:46:46 +0800

    update: 2024-01-03 08:46:43

---
+++ 2024-01/2024-01-03_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://thepihut.com/blogs/raspberry-pi-roundup/the-best-raspberry-pi-5-cooling-cases-and-heatsinks","The Best Raspberry Pi 5 Cooling Cases and Heatsinks","2024-01-03","The Pi Hut","0",],
+["https://thepihut.com/blogs/raspberry-pi-tutorials/how-to-use-two-camera-modules-with-raspberry-pi-5","How to use Two Camera Modules with Raspberry Pi 5","2024-01-03","The Pi Hut","0",],
+["https://thepihut.com/blogs/raspberry-pi-tutorials/raspberry-pi-pico-getting-started-guide","Raspberry Pi Pico Getting Started Guide","2024-01-03","The Pi Hut","0",],
+["https://www.solidot.org/story?sid=77031","Scribus 1.6.0 释出","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77030","荷兰限制向中国出口深紫外光刻机","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77029","Blender 诞生三十周年","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77028","AI 是否在浪费每一个人的时间？","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77027","尼康、索尼和佳能用相机数字签名对抗 AI 作假","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77026","Steam 停止支持 Windows 7、Windows 8 和 8.1","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77025","Gentoo 提供二进制安装选项","2024-01-03","Solidot","1",],
+["https://www.solidot.org/story?sid=77023","X5 级耀斑喷出的日冕物质抛射今天掠过地球","2024-01-03","Solidot","1",],
+];
