c71809480f52fe08d5cdaa4d63f6c8ae1b746b35
jinlei <wy7305e@hotmail.com>
2024-01-08 12:24:26 +0800

    update: 2024-01-08 12:24:25

--- 2023-12/2023-12-11_www.js
+++
@@ -1,6 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=76845","2024 年全球气温可能首次比工业化前高出 1.5℃","2023-12-11","Solidot","1",],
-["https://www.solidot.org/story?sid=76846","Linus Torvalds 谈 Linux 现状","2023-12-11","Solidot","1",],
-["https://www.solidot.org/story?sid=76847","研究人员披露专门针对泰国的 Linux 秘密恶意程序","2023-12-11","Solidot","1",],
-["https://www.solidot.org/story?sid=76848","哈勃望远镜在陀螺仪问题后恢复科学观测","2023-12-11","Solidot","1",],
-];
--- 2023-12/2023-12-22_www.js
+++
@@ -1,8 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=76941","纽约市当选议员承认用 AI 回答问题","2023-12-22","Solidot","1",],
-["https://www.solidot.org/story?sid=76943","澳大利亚土著基因非常独特","2023-12-22","Solidot","1",],
-["https://www.solidot.org/story?sid=76944","英国最高法院裁决 AI 不能成为专利的发明人","2023-12-22","Solidot","1",],
-["https://www.solidot.org/story?sid=76945","ULA 准备发射它的首枚 Vulcan 火箭","2023-12-22","Solidot","1",],
-["https://www.solidot.org/story?sid=76946","三大成人网站在欧盟面临更严厉的监管","2023-12-22","Solidot","1",],
-["https://www.solidot.org/story?sid=76947","科学家发现猫咪也会玩接球游戏","2023-12-22","Solidot","1",],
-];
