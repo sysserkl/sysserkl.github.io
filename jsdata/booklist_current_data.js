4324f37e6e1bc29c1c6528f2b17d39a457bd6ebc
jinlei <wy7305e@hotmail.com>
2024-01-25 21:26:43 +0800

    update: 2024-01-25 21:26:40

--- jsdata/booklist_current_data.js
+++ jsdata/booklist_current_data.js
@@ -1,4 +1,5 @@
 var csbooklist_source_global=[
+["da_dai_li_ji_zhong_gzxsdzhjhj_114759", "大戴礼记(中国哲学书电子化计划)","已整理","3",""],
 ["twenty_thousand_leagues_under_the_sautotw_6548","Twenty Thousand Leagues Under the Seas: An Underwater Tour of the World","englishwords,已整理","3",""],
 ["adventures_of_huckleberry_finn_2913", "Adventures of Huckleberry Finn(Mark Twain)","已整理,englishwords","3",""],
 ["essays_of_michel_de_montaigne_c_11799", "Essays of Michel de Montaigne — Complete(Michel de Montaigne)","已整理,englishwords","3",""],
---
+++ jsdoc_attachment/digest3/da_dai_li_ji_zhong_gzxsdzhjhj_114759_digest.js
@@ -0,0 +1,1 @@
+var digest_global=``;
