691408f4a55c4b5e3143a91fa3b173505f40e4ce
jinlei <wy7305e@hotmail.com>
2024-02-01 08:37:54 +0800

    update: 2024-02-01 08:37:49

---
+++ 2024-02/2024-02-01_www.js
@@ -0,0 +1,11 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=77264","Raspberry Pi 计划在伦敦 IPO","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77265","俄罗斯遭遇大规模网络故障","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77266","微软游戏业务收入超过了 Windows 部门","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77267","Instagram Threads 下载量位居苹果 Google 应用商店前十","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77268","德国警方扣押了价值 20 亿欧元的比特币","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77269","Google 过去一年减少了 7700 名员工","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77270","ICANN 提议内部使用的新顶级域名.INTERNAL","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77272","网信办去年约谈了逾万家网站","2024-02-01","Solidot","1",],
+["https://www.solidot.org/story?sid=77273","研究人员尝试将大肠杆菌作为显示器去玩《Doom》","2024-02-01","Solidot","1",],
+];
