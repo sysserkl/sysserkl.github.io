8ce7209045ddb79962463c6c583ec064f9810455
jinlei <wy7305e@hotmail.com>
2024-02-07 09:04:12 +0800

    update: 2024-02-07 09:04:06

---
+++ 2024-02/2024-02-07_www.js
@@ -0,0 +1,9 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=77321","Google 向 Rust 基金会资助了 100 万美元","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77320","Hugging Face 发布开源自定义 AI 聊天助手","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77319","GitHub 短暂屏蔽了它的联合创始人、前 CEO 的账号","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77317","人类对温度变化非常敏感","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77316","研究估计仙女星系质量约为太阳的 1.14 万亿倍","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77315","六成 Steam 玩家仍然用 1080p 显示屏分辨率","2024-02-07","Solidot","1",],
+["https://www.solidot.org/story?sid=77314","骗子使用 deepfake 冒充 CFO 骗走 2 亿港币","2024-02-07","Solidot","1",],
+];
