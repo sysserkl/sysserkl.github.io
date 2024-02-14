ec34fdb6991d035eecbff81b8d4230ad5248859c
jinlei <wy7305e@hotmail.com>
2024-02-14 14:44:10 +0800

    update: 2024-02-14 14:44:03

--- 2024-01/2024-01-11_www.js
+++
@@ -1,9 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=77103","爱情如何改变我们的大脑","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77102","乌克兰黑客报复俄罗斯破坏莫斯科 ISP","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77101","卫星引发的新“星球大战”","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77100","NASA 推迟阿耳忒弥斯月球载人飞行任务一年","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77099","Netflix 公布《三体》预告片","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77098","2023 年是有记录以来最热的一年","2024-01-11","Solidot","1",],
-["https://www.solidot.org/story?sid=77097","HPE 以 140 亿美元收购瞻博网络","2024-01-11","Solidot","1",],
-];
--- 2024-01/2024-01-13_www.js
+++
@@ -1,9 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=77122","深圳公司推出预装 SteamOS 的掌机","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77121","Meta 的 Threads 为何要整合联邦宇宙","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77119","联合国警告全球数十亿人面临干旱危机","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77118","报道称 Stuxnet 蠕虫是通过水泵渗透到伊朗核电站的","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77117","东京证券交易所总市值超过上交所","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77116","租车公司 Hertz 出售电动车购买汽油车","2024-01-13","Solidot","1",],
-["https://www.solidot.org/story?sid=77115","不同视频串流服务和地理位置限制让盗版再次流行","2024-01-13","Solidot","1",],
-];
