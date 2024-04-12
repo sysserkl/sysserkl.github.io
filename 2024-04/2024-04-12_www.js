8899bd02546448095d6d9b7606aa9861de199ed9
jinlei <wy7305e@hotmail.com>
2024-04-12 09:01:29 +0800

    update: 2024-04-12 09:01:15

---
+++ 2024-04/2024-04-12_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=77848","Gentoo Linux 成为 SPI 的关联项目","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77847","欧盟的 DMA 法律增加了小众浏览器的市场份额","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77846","美国 Delta IV 火箭完成最后一次发射","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77845","新法案要求美国 AI 公司披露其使用的版权材料","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77844","旧金山捷运计划在六年内淘汰 5.25 英寸软盘","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77843","沙特缩减筹划中的超级城市规模","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77842","美国电影协会计划再次加大力度打击盗版","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77841","拜登考虑撤销对 Julian Assange 指控的要求","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77840","Kobo 推出首款彩色电子书阅读器","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77839","2023 年图灵奖授予了以色列数学家 Avi Wigderson","2024-04-12","Solidot","1",],
+["https://www.solidot.org/story?sid=77838","X 自动将用户帖子里的 Twitter.com 改为 X.com","2024-04-12","Solidot","1",],
+];
