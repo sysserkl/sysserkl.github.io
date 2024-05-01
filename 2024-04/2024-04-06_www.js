8153ee22f2d199afd095c0bfafda30dd2a9e9870
jinlei <wy7305e@hotmail.com>
2024-05-01 08:15:12 +0800

    update: 2024-05-01 08:15:08

--- 2024-04/2024-04-06_www.js
+++
@@ -1,7 +0,0 @@
-var sourcelist=[
-["https://www.usgs.gov/news/news-releases#main-content","Skip to main content","2024-04-06","USGS","0",],
-["https://www.usgs.gov/news/state-news-release/media-alert-usgs-field-crews-study-avalanche-prone-regions-using-airborne","Media Alert: USGS field crews to study avalanche prone regions using airborne techniques near Durango","2024-04-06","USGS","0",],
-["https://www.usgs.gov/news/national-news-release/monarch-butterfly-decline-mystery-scientists-rule-out-habitat-loss","In monarch butterfly decline mystery, scientists rule out habitat loss in migration zone","2024-04-06","USGS","0",],
-["https://www.usgs.gov/news/national-news-release/illegal-oil-and-gas-wastewater-dumps-disrupt-foundations-fragile-desert","Illegal Oil and Gas Wastewater Dumps Disrupt Foundations of Fragile Desert Ecosystems","2024-04-06","USGS","0",],
-["https://www.usgs.gov/usgs-vulnerability-disclosure-policy","Vulnerability Disclosure Policy","2024-04-06","USGS","0",],
-];
--- 2024-04/2024-04-14_www.js
+++
@@ -1,6 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=77877","去年全国可能只有 4 个省人口正增长","2024-04-14","Solidot","1",],
-["https://www.solidot.org/story?sid=77876","Adobe 以每分钟约 3 美元的价格购买视频构建文本视频 AI 模型","2024-04-14","Solidot","1",],
-["https://www.solidot.org/story?sid=77875","政府要求电信公司逐步淘汰网络核心使用的外国芯片","2024-04-14","Solidot","1",],
-["https://www.solidot.org/story?sid=77874","LastPass 称黑客冒充 CEO 的声音试图欺骗其雇员","2024-04-14","Solidot","1",],
-];
