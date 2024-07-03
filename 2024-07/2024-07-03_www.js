2edff4279b4984ca524918b5c7c0071e90c0ca35
jinlei <wy7305e@hotmail.com>
2024-07-03 08:54:27 +0800

    update: 2024-07-03 08:54:18

---
+++ 2024-07/2024-07-03_www.js
@@ -0,0 +1,11 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=78575","微信微博等社媒平台宣布加强整治反日言论","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78576","南非研究员使用放射性技术遏制犀牛偷猎","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78577","欧盟竞争专员认为苹果不在欧盟推出 AI 是反竞争行为","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78578","法国监管机构准备指控英伟达反竞争","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78579","研究称间歇性禁食有助于改善认知","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78580","10 岁阿根廷男孩成为最年轻国际象棋大师","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78582","Google Chrome 不再信任 Entrust 签发的证书","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78583","Windows 11 中国政府版没有 Edge 等微软预装应用","2024-07-03","Solidot","1",],
+["https://www.solidot.org/story?sid=78584","科学家被控伪造数据以获取 1600 万美元的研究拨款","2024-07-03","Solidot","1",],
+];
