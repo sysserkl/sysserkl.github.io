075f5a4a8303ca5164d01fc69095518b55cd5568
jinlei <wy7305e@hotmail.com>
2024-07-07 09:06:46 +0800

    update: 2024-07-07 09:06:42

---
+++ 2024-07/2024-07-07_www.js
@@ -0,0 +1,5 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=78622","伊顿公学将禁止新生用智能手机","2024-07-07","Solidot","1",],
+["https://www.solidot.org/story?sid=78623","IERS 宣布 2024 年世界时将不会增加闰秒","2024-07-07","Solidot","1",],
+["https://southeastasiaglobe.com/cambodian-monkey-trade-with-canada/","Cambodian monkey exports to Canada for lab tests are surging, fueling health concerns","2024-07-07","Southeast Asia Globe","0",],
+];
