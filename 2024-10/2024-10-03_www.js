688991e47d42939149c08398778e79b1ec57e3f7
jinlei <wy7305e@hotmail.com>
2024-10-03 08:54:27 +0800

    update: 2024-10-03 08:54:25

---
+++ 2024-10/2024-10-03_www.js
@@ -0,0 +1,4 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=79399","研究人员培育出能抵抗 TR4 的香蕉品种","2024-10-03","Solidot","1",],
+["https://www.solidot.org/story?sid=79398","Windows 11 可选更新 KB5043145 导致了大量问题","2024-10-03","Solidot","1",],
+];
