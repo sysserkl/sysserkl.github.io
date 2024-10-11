52219a72d1602aaa32d75393f0553ea96727f94e
jinlei <wy7305e@hotmail.com>
2024-10-11 11:32:16 +0800

    update: 2024-10-11 11:32:14

---
+++ 2024-10/2024-10-11_www.js
@@ -0,0 +1,9 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=79444","WordPress.org 登陆页面加入 WP Engine 非隶属关系声明","2024-10-11","Solidot","1",],
+["https://www.solidot.org/story?sid=79445","互联网档案馆用户数据泄露","2024-10-11","Solidot","1",],
+["https://www.solidot.org/story?sid=79446","人类造成的物种灭绝影响超过预期","2024-10-11","Solidot","1",],
+["https://www.solidot.org/story?sid=79447","新晋诺奖得主称赞其学生解雇了 Sam Altman","2024-10-11","Solidot","1",],
+["https://www.solidot.org/story?sid=79448","Zoom 将让 AI 化身代替你出席虚拟会议","2024-10-11","Solidot","1",],
+["https://www.solidot.org/story?sid=79449","Linus Torvalds 建议内核开发者用主动语态写合并请求","2024-10-11","Solidot","1",],
+["https://www.oulu.fi/en/research/graduate-school/how-apply-doctoral-education","How to apply to doctoral education","2024-10-11","University of Oulu","0",],
+];
