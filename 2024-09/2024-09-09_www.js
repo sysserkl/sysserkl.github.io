42934d397f841a75d01fb80f3eb00116eb8a9524
jinlei <wy7305e@hotmail.com>
2024-10-16 11:43:19 +0800

    update: 2024-10-16 11:43:17

--- 2024-09/2024-09-09_www.js
+++
@@ -1,7 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=79192","Linux Man pages 维护者因经济压力暂停工作","2024-09-09","Solidot","1",],
-["https://www.solidot.org/story?sid=79191","马来西亚命令 ISP 重路由 DNS 流量，引发争议后暂停执行","2024-09-09","Solidot","1",],
-["https://www.solidot.org/story?sid=79190","Starlink 卫星占到了全部活跃卫星的三分之二","2024-09-09","Solidot","1",],
-["https://www.solidot.org/story?sid=79189","Z 世代精通计算机但不会盲打","2024-09-09","Solidot","1",],
-["https://www.solidot.org/story?sid=79187","波音 Starliner 飞船返回地面","2024-09-09","Solidot","1",],
-];
--- 2024-09/2024-09-15_www.js
+++
@@ -1,9 +0,0 @@
-var sourcelist=[
-["https://www.solidot.org/story?sid=79265","哈勃发现一对超大质量黑洞","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79264","Haiku 释出 R1/Beta5","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79263","美国在线约会与收入不平等相关","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79262","大模型的幻觉是不可避免地","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79258","130 万 Android 机顶盒被植入后门","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79257","FSR4 将是基于 AI 的上采样技术","2024-09-15","Solidot","1",],
-["https://www.solidot.org/story?sid=79255","美国计划限制希音和 Temu 利用小额免税规定","2024-09-15","Solidot","1",],
-];
