a016ceed9be2fd8b36073de3792a956cf1aa8eec
jinlei <wy7305e@hotmail.com>
2024-10-20 08:54:38 +0800

    update: 2024-10-20 08:54:36

---
+++ 2024-10/2024-10-20_www.js
@@ -0,0 +1,7 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=79537","OpenAI 相对于其它 AI 公司的优势基本消失","2024-10-20","Solidot","1",],
+["https://www.solidot.org/story?sid=79536","古巴电网故障导致千万人断电","2024-10-20","Solidot","1",],
+["https://www.solidot.org/story?sid=79535","美国越来越多的父母拒绝给孩子接种疫苗","2024-10-20","Solidot","1",],
+["https://www.solidot.org/story?sid=79538","德国主权科技基金过去两年向开源项目资助了逾  2300 万欧元","2024-10-20","Solidot","1",],
+["https://www.solidot.org/story?sid=79539","82 岁老妇仍然骑 13 岁时父母送的自行车","2024-10-20","Solidot","1",],
+];
