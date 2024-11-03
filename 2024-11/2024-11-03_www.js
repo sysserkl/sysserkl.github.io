6e1c33750d17a26ae6d40ab4ab1878d50e9127f4
jinlei <wy7305e@hotmail.com>
2024-11-03 09:01:02 +0800

    update: 2024-11-03 09:01:00

---
+++ 2024-11/2024-11-03_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=79661","Linus Torvalds 用电动汽车取代了燃油汽车","2024-11-03","Solidot","1",],
+["https://www.solidot.org/story?sid=79662","英伟达取代英特尔进入道琼斯工业平均指数","2024-11-03","Solidot","1",],
+["https://www.solidot.org/story?sid=79663","日本东京高院裁定不承认同性婚姻违宪","2024-11-03","Solidot","1",],
+["https://www.solidot.org/story?sid=79664","苹果收购图像编辑应用 Pixelmator","2024-11-03","Solidot","1",],
+["https://blog.heroku.com/building-supercharged-agents-heroku-agentforce","Building Supercharged Agents with Heroku and Agentforce","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/best-practices-optimizing-enterprise-cloud-applications-new-relic","Best Practices for Optimizing Your Enterprise Cloud Applications with New Relic","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/electron-on-heroku","Electron on Heroku","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/heroku-acm-now-supports-wildcard-domains","Simplify Your Cloud Security: Heroku ACM Now Supports Wildcard Domains","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/testing-react-app-chrome-heroku-ci","Testing a React App in Chrome with Heroku CI","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/heroku-dreamforce-2024","Discover Heroku at Dreamforce 2024","2024-11-03","Heroku Blog","0",],
+["https://blog.heroku.com/updating-twelve-factor-call-for-participation","Updating Twelve-Factor: A Call for Participation","2024-11-03","Heroku Blog","0",],
+];
