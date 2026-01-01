ebb4f5bf89f271cd639230d8397aaa8dbccbed3c
jinlei <wy7305e@hotmail.com>
2026-01-01 08:10:52 +0800

    update: 2026-01-01 08:10:51

---
+++ 2026-01/2026-01-01_www.js
@@ -0,0 +1,13 @@
+var sourcelist=[
+["https://www.solidot.org/story?sid=83183","Meta 以约 20 亿美元收购中资背景的 AI 公司 Manus","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83184","Ocean Infinity 准备对失踪 12 年的 MH370 展开新一轮搜索","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83185","黑客利用育碧 MongoDB 软件漏洞窃取源代码","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83186","俄罗斯入侵乌克兰改变了乌克兰人的成人内容消费习惯","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83187","F-Droid 升级了服务器","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83188","中国癌症新药研发数量全球居首","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83189","冰岛经历了有记录以来最暖和的平安夜","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83190","印度 GDP 超过日本成为全球第四大经济体","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83191","以色列部署无人机激光防御系统","2026-01-01","Solidot","1",],
+["https://www.solidot.org/story?sid=83192","外国科技从业者正在避开美国","2026-01-01","Solidot","1",],
+["https://www.cloudflare.com/5xx-error-landing?utm_source=errorcode_522&utm_campaign=www.listal.com","cloudflare.com","2026-01-01","Listal","0",],
+];
