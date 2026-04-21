77b7e6408fe9c05318557d1f29d3a1af7b4d0996
jinlei <wy7305e@hotmail.com>
2026-04-21 08:52:46 +0800

    update: 2026-04-21 08:52:43

--- hard_ln_io/klbase_tts.js
+++ hard_ln_io/klbase_tts.js
@@ -135,6 +135,11 @@ function init_temp_txt_share_b(is_php=true,menu_more='',do_add=true,is_simple=fa
     op.innerHTML=`
     <span class="aclick" onclick="textarea_shift_b('textarea_temp_txt_share','textarea_preparation_tts');">对调</span>
     <span class="aclick" onclick="textarea_diff_txt_share_b('textarea_temp_txt_share','textarea_preparation_tts');">diff</span>`;
+    
+    var otextarea=document.getElementById('textarea_temp_txt_share');
+    if (otextarea){
+        otextarea.style.height=(ismobile_b()?8:20)+'rem';
+    }
 }
 
 function spilt_rows_temp_txt_share_b(separation='\n'){
